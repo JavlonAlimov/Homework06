24706e2ff8b0c66a0a3c259bd3e3c496f494aad4
Javlon Alimov <javlonalimov1@gmail.com>
2023-07-19 16:37:16 +0500

    Longest word first change

--- Homework06/Program.cs
+++ Homework06/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace Homework06
+﻿using System.Text;
+
+namespace Homework06
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter the sentense : ");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(new char[] { ' ',',', '.', '!', '?', '$', '%', '@' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string longestWord = words[0];
+
+            for(int i  = 1 ; i < words.Length; i++) 
+            {
+                if (words[i].Length >= longestWord.Length) { 
+                    longestWord = words[i];
+                }
+            }
+            Console.WriteLine($"The longest word in this sentence is {longestWord} and it's length is {longestWord.Length}");
         }
     }
 }
